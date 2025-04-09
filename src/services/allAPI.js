5d42d185c6b514dde758bf16162d6312c31471ba
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-04-09 10:06:31 +0530

    axios done

---
+++ src/services/allAPI.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const commonAPI = async(httpMethod,url,reqBody,reqHeader)=>{
+    const reqConfig ={
+        method:httpMethod,
+        url,
+        data:reqBody,
+        headers:reqHeader? reqHeader :{"Content-Type":"application/json"}
+    }
+    return await axios(reqConfig).then(res=>{
+        return res
+    }).catch(err=>{
+        return err
+    })
+}
+export default commonAPI
