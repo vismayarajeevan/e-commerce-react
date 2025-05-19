01d46b294219ec7d078a397e2f692278b0aa9ef9
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-05-19 12:45:16 +0530

    server url updated

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,5 @@
-const SERVER_URL ="http://localhost:3000"
+// const SERVER_URL ="http://localhost:3000"
+
+const SERVER_URL ="https://e-commerce-backend-3023.onrender.com"
+
 export default SERVER_URL;
